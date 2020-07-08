f1db4406aa577c2764a0d0a24edab4bc87fb28c8
nkrishnan94 <npk13@case.edu>
2020-07-08 15:46:25 -0400

    fixed migration

--- eff_pop_2d.cpp
+++ eff_pop_2d.cpp
@@ -13,14 +13,16 @@
 #include <gsl/gsl_randist.h>
 
 
-unsigned long K  = 1000;
-unsigned int n_gens = 100;
-const int n_demes = 1000; 
+unsigned long K  = 3000;
+unsigned int n_gens = 3000;
+const int n_demes = 400; 
 const unsigned int n_spec = 2;
 float M = 0.25;
-float B = 2;
+float B = 8;
 float g0 = 0.01;
 unsigned long prof_hist = 0;
+unsigned long fast_samp_flag = 1;
+
 
 
 double sumDeme(long double arr[][n_demes][n_spec], int arrSize){
@@ -87,11 +89,14 @@ long double deme_aux[n_demes][n_demes][n_spec] = {{0}};
 
 
 
+
+
+
 int main (int argc, char * argv[]){
 	using namespace std;
 
 	int c;
-    while ((c = getopt (argc, argv, "K:Z:B:M:G")) != -1)
+    while ((c = getopt (argc, argv, "K:Z:B:M:G:F")) != -1)
     {
         if (c == 'K')
             K  = atoi(optarg); // carrying capacity
@@ -103,8 +108,16 @@ int main (int argc, char * argv[]){
             M = atof(optarg); // migration probability
         else if (c == 'G')
             g0 = atof(optarg); // growth rate
+        else if (c == 'F')
+            fast_samp_flag = atoi(optarg); // growth rate
 
     }
+    if ((B >= 2))
+    n_gens = 1*K;
+
+
+	if ((B < 2))
+	    n_gens = 15*int(sqrt(K));
 
 
 	const gsl_rng_type * T;
@@ -173,7 +186,7 @@ int main (int argc, char * argv[]){
 
 
 
-	for(int i = 0; i < int(n_demes*.5); i++){
+	/*for(int i = 0; i < int(n_demes*.5); i++){
 		for(int j = 0; j < int(n_demes); j++){
 
 		deme[i][j][0] = .5*K;
@@ -183,7 +196,7 @@ int main (int argc, char * argv[]){
 		}
 
 
-	}
+	}*/
 	//initial population in middle
 	deme[int(n_demes/2)][int(n_demes/2)][0] = .5*K;
 	deme[int(n_demes/2)][int(n_demes/2)][1] = .5*K;
@@ -191,14 +204,19 @@ int main (int argc, char * argv[]){
 
 
 
+
+
 	for (int dt = 0 ; dt < n_gens; dt++ ){
+
 		
 		for(int ii = 0; ii < int(n_demes); ii++){
 			for(int jj = 0; jj < int(n_demes); jj++){
 
 
-			deme_aux[ii][jj][0] = deme[ii][jj][0];
-			deme_aux[ii][jj][1] = deme[ii][jj][1];
+				deme_aux[ii][jj][0] = deme[ii][jj][0];
+				deme_aux[ii][jj][1] = deme[ii][jj][1];
+
+			
 
 			}
 
@@ -211,45 +229,64 @@ int main (int argc, char * argv[]){
 				int arr[2] = {i, j};
 				int neighb_vec[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
 				int neighbs[4][2];
+				int pop_sum = fast_samp_flag;
 
 				for(int ne=0; ne <4; ne++){
 					neighbs[ne][0] = (arr[0] + n_demes+neighb_vec[ne][0]) % n_demes;
 					neighbs[ne][1] = (arr[1] + n_demes+neighb_vec[ne][1]) % n_demes;
+					pop_sum+=deme_aux[neighbs[ne][0]][neighbs[ne][1]][0] +deme_aux[neighbs[ne][0]][neighbs[ne][1]][1];
+					pop_sum+=deme[neighbs[ne][0]][neighbs[ne][1]][0] +deme[neighbs[ne][0]][neighbs[ne][1]][1];
+
+					/*int n_neighbs[4][2];
+					int ne_arr[2] = {neighbs[ne][0], neighbs[ne][1]};
+
+					
+					for(int ne_=0; ne_ <4; ne_++){
+						n_neighbs[ne_][0] = (ne_arr[0] + n_demes+neighb_vec[ne_][0]) % n_demes;
+						n_neighbs[ne_][1] = (ne_arr[1] + n_demes+neighb_vec[ne_][1]) % n_demes;
+						pop_sum+=deme_aux[n_neighbs[ne_][0]][n_neighbs[ne_][1]][0] + deme_aux[n_neighbs[ne_][0]][n_neighbs[ne_][1]][1];
+						pop_sum+=deme[n_neighbs[ne_][0]][n_neighbs[ne_][1]][0] + deme[n_neighbs[ne_][0]][n_neighbs[ne_][1]][1];
+
+					}*/
+
+
+
+
+				}
+
+
+				if (((deme[i][j][0] + deme[i][j][1]+deme_aux[i][j][1]+deme_aux[i][j][0]) !=0) || (pop_sum != 0)){
+
+					long double f1 = deme[i][j][0]/int(K);
+					long double f2 = deme[i][j][1]/int(K);
+					f1 = (1 - M)*f1;
+					f2 = (1 - M)*f2; 
+					for(int ne = 0; ne <4; ne++){
+
+						f1+= (M/4)*deme_aux[neighbs[ne][0]][neighbs[ne][1]][0]/int(K);
+						f2+= (M/4)*deme_aux[neighbs[ne][0]][neighbs[ne][1]][1]/int(K);
+
+
+					}
+
+					w_v = 1 - g0*(1+ B*(f1+f2));
+					w_avg = w_v + (w_s - w_v)*(f1 + f2);
+
+					f1 *= w_s/w_avg;
+					f2 *= w_s/w_avg;
+					new_prob[0] = 1-f1-f2;
+					new_prob[1] = f1;
+					new_prob[2] = f2;
+					gsl_ran_multinomial(r,n_spec+1,K,new_prob,new_cnt);
+					deme[i][j][0] = new_cnt[1];
+					deme[i][j][1] = new_cnt[2];
+
 
 
 
 				}
 
 
-	
-
-
-
-
-
-				long double f1 = deme[i][j][0]/int(K);
-				long double f2 = deme[i][j][1]/int(K);
-				f1 = (1 - M)*f1;
-				f2 = (1 - M)*f2; 
-				for(int ne = 0; ne <4; ne++){
-
-					f1+= (M/4)*deme_aux[neighbs[ne][0]][neighbs[ne][1]][0]/int(K);
-					f2+= (M/4)*deme_aux[neighbs[ne][0]][neighbs[ne][1]][1]/int(K);
-
-
-				}
-
-				w_v = 1 - g0*(1+ B*(f1+f2));
-				w_avg = w_v + (w_s - w_v)*(f1 + f2);
-
-				f1 *= w_s/w_avg;
-				f2 *= w_s/w_avg;
-				new_prob[0] = 1-f1-f2;
-				new_prob[1] = f1;
-				new_prob[2] = f2;
-				gsl_ran_multinomial(r,n_spec+1,K,new_prob,new_cnt);
-				deme[i][j][0] = new_cnt[1];
-				deme[i][j][1] = new_cnt[2];
 
 				
 
